aefb05f3b780603fd54132f55063ac10de79dd0d
akash.shukla0203 <akash.shukla0203@gmail.com>
2022-08-01 02:28:44 +0530

    assign

---
+++ entrycontrolloops.cpp
@@ -0,0 +1,94 @@
+#include<iostream> 
+using namespace std;
+
+// int main()
+// {
+// float i,n,pos=0,neg=0,neu=0;
+
+
+// for(i=1;i<=10;i++)
+// {
+//     cout << endl << "Enter the number :";
+//     cin>>n;
+    
+//     if(n<0){
+//         neg++;
+//     }else if (n > 0) {
+//         pos++;
+//     }
+//     else{
+//         neu++;
+//     }
+// }
+// cout<<endl<<"Total postive count:"<<pos;
+// cout<<endl<<"Total negative count:"<<neg;
+// cout<<endl<<"Total zero count"<<neu;
+
+// return 0;
+// }
+
+// int main()
+// {
+// float i,n,total=0,avg;
+
+
+// for(i=1;i<=10;i++)
+// { 
+//     cout << endl << "Enter the number :";
+//     cin>>n;
+//     total=total+n;
+// }
+// avg=total/10;
+// cout<<endl<<"Total of no.:"<<total;
+// cout<<endl<<"Avg of no.:"<<avg;
+// return 0;
+// }
+
+// int main()
+// {
+// float i,n;
+
+// cout << endl << "Enter the number :";
+// cin>>n;
+
+// for(i=1;i<=10;i++)
+// { 
+//    cout<<endl<<n <<" X "<< i<<"="<<n*i;
+// }
+// return 0;
+// }
+
+// int main()
+// {
+// float i,n,fact=1;
+
+// cout << endl << "Enter the number :";
+// cin>>n;
+
+// for(i=1;i<=n;i++)
+// { 
+//    fact=fact*i;
+// }
+
+// cout<<endl<<"Factorial of "<< n <<" is "<<fact;
+// return 0;
+// }
+
+// int main()
+// {
+// int i,n;
+
+// int t1=1,t2=2;
+// int next=t1+ t2;
+
+// cout<<endl<<"Enter the no. of terms :";
+// cin>>n;
+// cout<<endl<<"Fibonacci series "<< t1 <<","<<t2;
+// for (i = 3; i <= n; ++i) {
+//     cout<<","<<next;
+//     t1 = t2;
+//     t2 = next;
+//     next = t1 + t2;
+//   }
+// return 0;
+// }
